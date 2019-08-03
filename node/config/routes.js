51e8a209c6d4937b50799dd26a4ef548c6c25b52
anil singh chawda <anilsinghchawda@gmail.com>
2019-08-03 14:24:06 +0530

    2/08 college

--- node/config/routes.js
+++ node/config/routes.js
@@ -5,6 +5,8 @@ var routes = express.Router();
 routes.use("/", require("../controllers/login"));
 routes.use("/check", require("../controllers/check"));
 routes.use("/addPro", require("../controllers/addPro"));
+routes.use("/showPro", require("../controllers/showPro"));
+
 
 routes.use("/category", require("../controllers/category"));
 routes.use("/product", require("../controllers/product"));
